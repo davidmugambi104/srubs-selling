79f853307182f695794065d376078fc95a5ed064
davidmugambi104 <davidmugambi104@gmail.com>
2024-08-06 01:17:26 +0300

    added about-page

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const bar =document.getElementById("bar");
-const close=document.getElementById("close");
-const nav =document.getElementById("navbar");
+document.addEventListener("DOMContentLoaded", function() {
+    const bar = document.getElementById("bar");
+    const close = document.getElementById("close");
+    const nav = document.getElementById("navbar");
 
-if(bar){
-    bar.addEventListener("click", ()=>{
-        nav.classList.add('active')
-    })
-}
-if(close){
-    close.addEventListener("click", ()=>{
-        nav.classList.remove('active')
-    })
-}
+    if (bar) {
+        bar.addEventListener("click", () => {
+            nav.classList.add('active');
+        });
+    }
+
+    if (close) {
+        close.addEventListener("click", () => {
+            nav.classList.remove('active');
+        });
+    }
+});
